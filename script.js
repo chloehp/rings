c0f60e2fbd5545fdea09cce40de4ed8dd1be4f93
Chloe Hughes-Penzer <chloe.hughespenzer@gmail.com>
2024-12-14 13:17:54 +0000

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("abc");
+
+//console.log("navigator.userAgent:");
+//console.log(navigator.userAgent);
+//console.log("navigator.appVersion:");
+//console.log(navigator.appVersion);
+//console.log("navigator.platform:");
+//console.log(navigator.platform);
+//console.log("navigator.language:");
+//console.log(navigator.language);
+
+console.log(history)
+  
