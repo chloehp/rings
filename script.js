3967a67890b4e3f390ed1ea0f48ee08a3b0c445c
Chloe Hughes-Penzer <chloe.hughespenzer@gmail.com>
2024-12-15 15:58:17 +0000

    fonts +

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-window.scroll(0, 300);
+window.scroll(0, 120);
+setInterval(scrollUpdate, 120);
 
 function scrollUpdate() {
-    const scrolled = document.documentElement.scrollTop / 30;
+    const scrolled = document.documentElement.scrollTop / 24;
     const rightring = document.getElementById("s006-001");
     const leftring = document.getElementById("s005-001");
     rightring.style.rotate = scrolled + "deg";
