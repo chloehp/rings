ee292807b0ea8f06fc18a179dd65f5cf3c877682
Chloe Hughes-Penzer <chloe.hughespenzer@gmail.com>
2024-12-20 00:41:02 +0000

    c

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 //window.scroll(0, 120);
 document.onreadystatechange = function () {
     if (document.readyState === "complete") {
-        document.getElementById("loady").style.scale = 0.15;
+        const loady = document.getElementById("loady");
+        loady.style.scale = 1;
+        setInterval(scrollUpdate, 120);
+        setTimeout(function(){
+            document.getElementById("s008-000-l").remove();
+            loady.remove();
+        }, 1800);
     }
 }
-setInterval(scrollUpdate, 120);
 
 function scrollUpdate() {
-    const scrolled = document.documentElement.scrollTop / 21;
+    const scrolled = document.documentElement.scrollTop / (6 + (screen.width / 150));
     const rightring = document.getElementById("s006-001");
     const leftring = document.getElementById("s005-001");
     rightring.style.rotate = scrolled + "deg";
