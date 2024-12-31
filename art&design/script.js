1df03cfb136ea24fa8ee6c0ff7a5c354697f066f
Chloe Hughes-Penzer <chloe.hughespenzer@gmail.com>
2024-12-31 14:56:44 +0000

    art and design

---
+++ art&design/script.js
@@ -0,0 +1,11 @@
+
+document.onreadystatechange = function () {
+    const ring = document.getElementById("s006-001");
+    const circle = document.getElementById("s006-000");
+    const heading = document.getElementById("s006-h");
+    setTimeout(function(){
+        ring.style.rotate = "540deg";
+        circle.style.opacity = "0.09";
+        setTimeout(function(){heading.style.visibility = "visible";}, 30);
+    }, 300);
+}
