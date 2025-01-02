e6d911181642790ef98a9ee989c5a2900bdb6484
Chloe Hughes-Penzer <chloe.hughespenzer@gmail.com>
2025-01-02 02:18:31 +0000

    tiktok embed

--- art&design/script.js
+++ art&design/script.js
@@ -1,11 +1,22 @@
+let hasTiktokEmbedLoaded = false;
 
 document.onreadystatechange = function () {
     const ring = document.getElementById("s006-001");
     const circle = document.getElementById("s006-000");
     const heading = document.getElementById("s006-h");
+    const tiktok = document.getElementById("tiktok-iframe");
     setTimeout(function(){
         ring.style.rotate = "420deg";
         circle.style.opacity = "0.09";
-        setTimeout(function(){heading.style.visibility = "visible";}, 30);
-    }, 300);
+        setTimeout(function(){heading.style.opacity = "1";}, 30);
+    }, 150);
+    setTimeout(function(){
+        if (hasTiktokEmbedLoaded !== true) {
+            tiktok.children[0].innerHTML = "Failed to load";
+            setTimeout(vanishElement, 900, tiktok);
+        }
+        else {
+            tiktok.children[0].innerHTML = "";
+        }
+    }, 9000);
 }
---
+++ global.js
@@ -0,0 +1,6 @@
+function vanishElement(el) {
+    console.log("vanishing element: " + el.id)
+    el.style.opacity = "0";
+    el.style.scale = "0.6";
+    setTimeout(function(){el.remove()}, 600);
+}
