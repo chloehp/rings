991c99d37f500e4dd5d1a960cee5e03536c289cc
Chloe Hughes-Penzer <chloe.hughespenzer@gmail.com>
2025-01-09 04:38:13 +0000

    loading elements

--- art&design/script.js
+++ art&design/script.js
@@ -1,24 +1,40 @@
 let hasTiktokEmbedLoaded = false;
+let readyStateHasChanged = false;
 
 document.onreadystatechange = function () {
-    const ring = document.getElementById("s006-001");
-    const circle = document.getElementById("s006-000");
-    const heading = document.getElementById("s006-h");
-    const tiktok = document.getElementById("tiktok-iframe");
-    setTimeout(function(){
-        ring.style.rotate = "420deg";
-        circle.style.opacity = "0.09";
-        setTimeout(function(){heading.style.opacity = "1";}, 30);
-    }, 150);
-    setTimeout(function(){
-        //console.log(tiktok.children[1].contentWindow.postMessage.name);
-        if (hasTiktokEmbedLoaded !== true) {
-            tiktok.children[0].innerHTML = "Failed to load.";
-            setTimeout(vanishElement, 900, tiktok);
-        }
-        else {
-            //tiktok.children[1].style.opacity = "1";
-            tiktok.children[0].innerHTML = "";
-        }
-    }, 12000);
+    if (readyStateHasChanged === false) {                                               // document is interactive
+        const ring = document.getElementById("s006-001");
+        const circle = document.getElementById("s006-000");
+        const heading = document.getElementById("s006-h");
+        const tiktok = document.getElementById("tiktok-iframe");
+        const sketchbookColumn = document.getElementById("sketchbook-column");
+        const everythingColumn = document.getElementById("everything-else-column");
+        console.log(document.readyState);
+
+        setTimeout(function(){
+            ring.style.rotate = "420deg";
+            circle.style.opacity = "0.09";
+            setTimeout(function(){heading.style.opacity = "1";}, 30);
+            tiktok.style.opacity = "1";
+            tiktok.style.scale = "1";
+
+            loadElement(artList, "sketchbook", sketchbookColumn);
+            loadElement(artList, "", everythingColumn);
+        }, 150);
+        setTimeout(function(){
+            //console.log(tiktok.children[1].contentWindow.postMessage.name);
+            if (hasTiktokEmbedLoaded !== true) {
+                tiktok.children[0].innerHTML = "Failed to load.";
+                //setTimeout(vanishElement, 900, tiktok);
+            }
+            else {
+                //tiktok.children[1].style.opacity = "1";
+                tiktok.children[0].innerHTML = "";
+            }
+        }, 9000);
+    }
+    else {
+        //console.log("document has finished loading");
+    }
+    readyStateHasChanged = true;
 }
--- global.js
+++ global.js
@@ -1,10 +1,55 @@
 function vanishElement(el) {
-    console.log("vanishing element: " + el.id)
+    console.log("vanish element: " + el.id)
     el.style.opacity = "0";
     el.style.scale = "0.6";
     setTimeout(function(){el.remove()}, 600);
 }
 
 function appirateElement(el) {
+    setTimeout(function(){
+        console.log("appirate element: " + el.id)
+        el.style.opacity = "1";
+        el.style.scale = "1";    
+    }, 300);
+}
+
+const haveIloaded = [];
+function loadElement(from, cat, into) {
+    const fromLen = from.length;
+    const columns = into.querySelectorAll(".content--load-here");
+    let columnIndex = 0;
+    let elementsAppended = 0;
+
+    haveIloaded.push(0);
+    const thisIterationOfHaveIloaded = haveIloaded.length - 1;
+
+    let i = 0;
+    const thisInterval = setInterval(function () {
+        if (cat === from[i].cat) {
+            const el = document.createElement("a");
+            const img = document.createElement("img");
+
+            el.setAttribute("class", "content--element");                       // set attributes
+            el.setAttribute("href", from[i].url);
+            el.setAttribute("target", "_blank");
+            img.setAttribute("alt", from[i].alt);
+            img.setAttribute("src", from[i].thumb);
+
+            img.setAttribute("onload", "haveIloaded[" + thisIterationOfHaveIloaded + "]++");    // when image loads, add 1 to the item of the array that represents this iteration of the loadElement function
+            el.appendChild(img);                                                // append img to element
+
+            if (columnIndex >= columns.length) {columnIndex = 0}                // iterate through columns
+
+            columns[columnIndex].appendChild(el);                               // append element to next column
+            appirateElement(el);
+            columnIndex++;
+            elementsAppended++;
+            console.log(elementsAppended + " load " + from[i].alt + " into " + into.id);
+        }        
+        
+        i++; 
+        if (i >= fromLen) {clearInterval(thisInterval)}
+    }, 150);
+
     
 }
