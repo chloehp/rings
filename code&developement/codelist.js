77d4d7acec676fcaf95e975f1de83c307aa122b8
Chloe Hughes-Penzer <chloe.hughespenzer@gmail.com>
2025-01-14 04:31:53 +0000

    convert to webp

--- code&developement/codelist.js
+++ code&developement/codelist.js
@@ -1,21 +1,21 @@
 const codeList = [
     {
-        thumb: "../assets/mid_1080p/filigree2.png",
-        url: "../assets/mid_1080p/filigree2.png",
+        thumb: "../assets/mid_1080p/filigree2.webp",
+        url: "../assets/mid_1080p/filigree2.webp",
         title: "Witch tools",
         desc: "",
         cat: "js",
     },
     {
-        thumb: "../assets/mid_1080p/filigree2.png",
-        url: "../assets/mid_1080p/filigree2.png",
+        thumb: "../assets/mid_1080p/filigree2.webp",
+        url: "../assets/mid_1080p/filigree2.webp",
         title: "Colour light",
         desc: "",
         cat: "",
     },
     {
-        thumb: "../assets/mid_1080p/filigree1.png",
-        url: "../assets/mid_1080p/filigree1.png",
+        thumb: "../assets/mid_1080p/filigree1.webp",
+        url: "../assets/mid_1080p/filigree1.webp",
         title: "Card flip",
         desc: "",
         cat: "js",
